ade9a980909cc90c33ca6107efe72ed53b1b8871
dvvss <davis@ak.com>
2024-04-13 13:17:30 +0800

    Add mock tokens

---
+++ contracts/MockUSDT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSDT is ERC20 {
+
+    constructor() ERC20("Mock USDT", "mUSDT") {
+        _mint(msg.sender, 50000000 * (10 ** uint256(decimals())));
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
